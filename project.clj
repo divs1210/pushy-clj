a940d88e8c2562b2df5683bdf7d5042ab9c69b66
Divyansh Prakash <divyansh@helpshift.com>
2016-05-26 17:33:15 +0530

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pushy-clj "0.2.0-SNAPSHOT"
+(defproject pushy-clj "0.2.1-SNAPSHOT"
   :description "A Clojure wrapper over Pushy for sending APNs push notifications."
   :url "https://github.com/divs1210/pushy-clj"
   :license {:name "Eclipse Public License"
