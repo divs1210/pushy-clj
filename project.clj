fc0d622c196cc74e589d15651dd1f9bdedced51b
Divyansh Prakash <divyansh@helpshift.com>
2016-08-01 11:31:23 +0530

    Version bump - 0.2.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject pushy-clj "0.2.1-SNAPSHOT"
+(defproject pushy-clj "0.2.1"
   :description "A Clojure wrapper over Pushy for sending APNs push notifications."
   :url "https://github.com/divs1210/pushy-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.eclipse.jetty.alpn/alpn-api "1.1.2.v20150522"]
                  [io.netty/netty-tcnative-boringssl-static "1.1.33.Fork17"]
                  [io.netty/netty-all "4.1.0.CR7"]
