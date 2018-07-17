68608f6278d2cd3a1412c360c2ede03308240968
Divyansh Prakash <divyansh.prakash@yahoo.com>
2018-07-17 17:08:19 +0530

    Fix #7 - IllegalStateException with old version of pushy

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject pushy-clj "0.4.0"
+(defproject pushy-clj "0.4.1"
   :description "A Clojure wrapper over Pushy for sending APNs push notifications."
   :url "https://github.com/divs1210/pushy-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.turo/pushy "0.11.3"]])
+                 [com.turo/pushy "0.13.3"]])
